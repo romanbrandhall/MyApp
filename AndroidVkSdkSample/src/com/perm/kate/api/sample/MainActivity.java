c36cecdb676605def9d9cc03746d382cf80218f2
romanbrandhall <roman.brandhall@gmail.com>
2015-02-23 02:03:37 +0300

    test desctop

--- AndroidVkSdkSample/src/com/perm/kate/api/sample/MainActivity.java
+++ AndroidVkSdkSample/src/com/perm/kate/api/sample/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends Activity {
     Account account=new Account();
     Api api;
 //	public volatile ArrayList<User> myUsers=new ArrayList<User>();
+    //myTestDecstop
 	public volatile User myUsr=new User();
     @Override
     public void onCreate(Bundle savedInstanceState) {
